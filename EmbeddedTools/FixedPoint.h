70b62fa38d22175c503df28f081c58be91a2ae81
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2019-08-27 21:51:47 -0300

    Create FixedPoint.h

---
+++ EmbeddedTools/FixedPoint.h
@@ -0,0 +1,70 @@
+//! EmbeddedTools Version 1.0b
+/*!
+  This code file was written by Jorge Henrique Moreira Santana and is under
+  the GNU GPLv3 license. All legal rights are reserved.
+
+  Permissions of this copyleft license are conditioned on making available
+  complete source code of licensed works and modifications under the same
+  license or the GNU GPLv3. Copyright and license notices must be preserved.
+  Contributors provide an express grant of patent rights. However, a larger
+  work using the licensed work through interfaces provided by the licensed
+  work may be distributed under different terms and without source code for
+  the larger work.
+
+  * Permissions:
+    -> Commercial use;
+    -> Modification;
+    -> Distribuition;
+    -> Patent Use;
+    -> Private Use;
+
+  * Limitations:
+    -> Liability;
+    -> Warranty;
+
+  * Conditions:
+    -> License and copyright notice;
+    -> Disclose source;
+    -> State changes;
+    -> Same license (library);
+
+  For more informations, check the LICENSE document. If you want to use a
+  commercial product without having to provide the source code, send an email
+  to jorge_henrique_123@hotmail.com to talk.
+*/
+
+#ifndef FixedPoint_H
+#define FixedPoint_H
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+#include "EmbeddedTools.h"
+
+#define AMOUNT_OF_FRACTIONARY_BITS                                21
+#define fixed_t                                                   int32_t
+
+#define uiRoundingFactor                                          (1 << (AMOUNT_OF_FRACTIONARY_BITS - 1))
+
+#define uiGetFixedMinimumNumber()                                 -(1 << ((((uint8_t) sizeof(fixed_t)) << 3) - AMOUNT_OF_FRACTIONARY_BITS - 1))
+#define fGetFixedResolution()                                     pow(2, -(AMOUNT_OF_FRACTIONARY_BITS - 1))
+#define fGetFixedMaximumNumber()                                  (-uiGetFixedMinimumNumber() - fGetFixedResolution())
+
+#define fxFloatToFixed(fNumber)                                   ((fixed_t) ((fNumber) * (float)(1 << (AMOUNT_OF_FRACTIONARY_BITS))))
+#define fFixedToFloat(fxNumber)                                   ((float) ((fxNumber) / (float) (1 << (AMOUNT_OF_FRACTIONARY_BITS))))
+#define uiFixedToInt(fxNumber)                                    ((fxNumber) >> AMOUNT_OF_FRACTIONARY_BITS)
+
+#define fxFixedAdd(...)                                           fxFixedAdd(sizeof((int []) {__VA_ARGS__}) / sizeof(int), __VA_ARGS__)
+#define fxFixedMultiply(...)                                      fxFixedMultiply(sizeof((int []) {__VA_ARGS__}) / sizeof(int), __VA_ARGS__)
+#define fxFixedDivide(...)                                        fxFixedDivide(sizeof((int []) {__VA_ARGS__}) / sizeof(int), __VA_ARGS__)
+
+fixed_t (fxFixedAdd)(int iAmountOfNumbers, ...);
+fixed_t (fxFixedMultiply)(int iAmountOfNumbers, ...);
+fixed_t (fxFixedDivide)(int iAmountOfNumbers, ...);
+
+#ifdef __cplusplus
+  }
+#endif
+
+#endif
