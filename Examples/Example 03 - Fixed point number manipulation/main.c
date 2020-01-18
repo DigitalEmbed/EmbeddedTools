9743f38c686bdaa77725928a7a52b4cc0c70b5a9
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2020-01-18 11:59:23 -0300

    Create main.c

---
+++ Examples/Example 03 - Fixed point number manipulation/main.c
@@ -0,0 +1,118 @@
+//! Example 03 - Fixed point number manipulation
+/*!
+  This simply example demonstrates the use of bit manipulation.
+
+  This code file was written by Jorge Henrique Moreira Santana and is under
+  the GNU GPLv3 license. All legal rights are reserved.
+
+  Permissions of this copyleft license are conditioned on making available
+  complete source code of licensed works and modifications under the same
+  license or the GNU GPLv3. Copyright and license notices must be preserved.
+  Contributors provide an express grant of patent rights. However, a larger
+  work using the licensed work through interfaces provided by the licensed
+  work may be distributed under different terms and without source code for
+  the larger work.
+
+  * Permissions:
+    -> Commercial use;
+    -> Modification;
+    -> Distribuition;
+    -> Patent Use;
+    -> Private Use;
+
+  * Limitations:
+    -> Liability;
+    -> Warranty;
+
+  * Conditions:
+    -> License and copyright notice;
+    -> Disclose source;
+    -> State changes;
+    -> Same license (library);
+
+  For more informations, check the LICENSE document. If you want to use a
+  commercial product without having to provide the source code, send an email
+  to jorge_henrique_123@hotmail.com to talk.
+*/
+
+#include <stdio.h>
+#include <EmbeddedTools.h>
+
+int main(){
+  /*!
+    A) Printing SoftDecimal configurations and limits...
+    Configure it on Configs.h file in EmbeddedTools folder.
+  */
+  printf("Maximum possible value: %f\n", (double) SoftDecimal_getMaximumValue());
+  printf("Minimum possible value: %f\n", (double) SoftDecimal_getMinimumValue());
+  printf("Resolution: %f\n\n", (double) SoftDecimal_getResolution());
+
+  /*!
+    B) Declaring a fixed point type variables...
+  */
+  fixed_t fxNumberA = newSoftDecimal(3.14);
+  fixed_t fxNumberB = newSoftDecimal(2.0);
+
+  /*!
+    C) Printing a fixed point type variable...
+  */
+  printf("Fixed point number A: %f\n", (double) SoftDecimal_toFloat(fxNumberA));
+  printf("Fixed point number B: %f\n\n", (double) SoftDecimal_toFloat(fxNumberB));
+
+  /*!
+    D) Changing fxNumberA value...
+  */
+  fxNumberA = SoftDecimal_toFixed(0.0);
+  printf("Fixed point number A changed to 0: %f\n\n", (double) SoftDecimal_toFloat(fxNumberA));
+
+  /*!
+    F) Summing...
+  */
+  fxNumberA = SoftDecimal_floatSum(3.14, 2.71);
+  printf("Fixed point number A summed with 3.14 and 2.71: %f\n\n", (double) SoftDecimal_toFloat(fxNumberA));
+
+  /*!
+    E) Subtracting...
+  */
+  fxNumberB = SoftDecimal_fixedSubtract(fxNumberB, fxNumberA);
+  printf("Fixed point number B subtracted with number A: %f\n\n", (double) SoftDecimal_toFloat(fxNumberB));
+
+  /*!
+    G) Other way to subtracting (or summing with a negative number)...
+  */
+  fxNumberB = SoftDecimal_fixedSum(fxNumberA, -(fxNumberB), SoftDecimal_toFixed(-1.5));
+  printf("Fixed point number B subtracted by number A and 1.5: %f\n\n", (double) SoftDecimal_toFloat(fxNumberB));
+
+  /*!
+    H) Multiplying...
+  */
+  fxNumberA = SoftDecimal_floatMultiply(SoftDecimal_toFloat(fxNumberA), 2.0, 3.5);
+  printf("Fixed point number A multiplied by 2 and 3.5: %f\n\n", (double) SoftDecimal_toFloat(fxNumberA));
+
+  /*!
+    I) Multiplying fixed_t types...
+  */
+  fxNumberB = SoftDecimal_fixedMultiply(fxNumberA, fxNumberB);
+  printf("Fixed point number A multiplied by number B: %f\n\n", (double) SoftDecimal_toFloat(fxNumberB));
+
+  /*!
+    J) Dividing...
+  */
+  fxNumberB = SoftDecimal_fixedDivide(fxNumberB, SoftDecimal_toFixed(2.0));
+  printf("Fixed point number B divided by 2: %f\n\n", (double) SoftDecimal_toFloat(fxNumberB));
+
+  /*!
+    K) Another way to divide (is more faster for CPU and more accurate, but is more "ugly")...
+  */
+  fxNumberA = SoftDecimal_floatMultiply(SoftDecimal_toFloat(fxNumberA), 0.5);
+  printf("Fixed point number A multiplied by 0.5 (or divided by 2): %f\n", (double) SoftDecimal_toFloat(fxNumberA));
+
+  /*!
+    L) Checking if the calculations were performed safely ...
+  */
+  if (SoftDecimal_error() == true){
+    printf("\nWarning! A limit has been reached! Probably, the calculations are wrongs!");
+    return -1;
+  }
+  return 0;
+}
