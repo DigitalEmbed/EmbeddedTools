9cea829710902cdb2209a2ce3ff79188ced4d214
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2020-01-20 00:16:45 -0300

    Update FixedPoint.c

--- EmbeddedTools/FixedPoint.c
+++ EmbeddedTools/FixedPoint.c
@@ -5,7 +5,6 @@
     &&(!defined(__SOFT_DECIMAL_SIZE_16_BIT__))\
     &&(!defined(__SOFT_DECIMAL_SIZE_32_BIT__))))
   #pragma message("Buffer manager disabled!")
-
 #else
 
 #if ((__AMOUNT_OF_FRACTIONARY_BITS__ > 0)\
