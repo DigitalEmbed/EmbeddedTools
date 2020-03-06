37870d35267280a3a817a805ba27300fc77e0df9
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2020-03-06 12:45:26 -0300

    Update main.c

--- Examples/Standard C/Example 03 - Fixed point number manipulation/main.c
+++ Examples/Standard C/Example 03 - Fixed point number manipulation/main.c
@@ -8,6 +8,7 @@
   Permissions of this copyleft license are conditioned on making available
   complete source code of licensed works and modifications under the same
   license or the GNU GPLv3. Copyright and license notices must be preserved.
+  
   Contributors provide an express grant of patent rights. However, a larger
   work using the licensed work through interfaces provided by the licensed
   work may be distributed under different terms and without source code for
@@ -126,7 +127,7 @@ int main(){
   /*!
     L) Checking if the calculations were performed safely ...
   */
-  if (SoftDecimal_error() == true){
+  if (SoftDecimal_checkError() == true){
     printf("\nWarning! A limit has been reached! Probably, the calculations are wrongs!\n");
     return -1;
   }
